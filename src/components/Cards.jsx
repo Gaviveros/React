3db28de14b773e214f9f5a4ae05638cf9f2a258f
BernardiniMatias <bernardinimatias14@gmail.com>
2022-06-05 12:08:16 -0300

    first

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,4 @@
 import kandinsky from '..img/kandinsky.png'
-import kline from '..img/kline.jpg'
-
 function Cards() {
 
     return (
@@ -8,8 +6,8 @@ function Cards() {
         <div className="card bg-dark text-white">
              <div className="card-img-overlay">
                 <h5 className="card-title">Kandinsky</h5>
-                <p className="card-text"></p>
-                <p className="card-text"></p>
+                <p className="card-text">q</p>
+                <p className="card-text">q</p>
             </div>
         </div>
       </>
